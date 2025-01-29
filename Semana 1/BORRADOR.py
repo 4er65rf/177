3c1f55e6b097aa48da2c27fc073b199ea639a854
4er65rf <aguilarosiel117@gmail.com>
2025-01-29 17:40:06 -0600

    Pruebas

--- Semana 1/BORRADOR.py
+++ Semana 1/BORRADOR.py
@@ -8,7 +8,7 @@ type ("11")
 type ("abcdef")
 print (1,000,000)
 """
-
+"""
 mensaje = ("Y ahora algo completamente diferente")
 n=17
 pi=3.1415926535897931
@@ -20,3 +20,16 @@ type(mensaje)
 print(1)
  x= 2
  print(x)
+ """
+"""
+hour=20
+minute=32 
+print(hour-1)
+print(hour*60+minute)
+print(minute/60)
+"""
+
+print(x=7)
+print(num= 5)
+print(x = 5)
+print ("x + 1")
