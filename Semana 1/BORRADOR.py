ef37e095dd0ae00189e663b50ac2b746c86ca741
4er65rf <aguilarosiel117@gmail.com>
2025-01-29 15:37:15 -0600

    Pruebas

--- Semana 1/BORRADOR.py
+++ Semana 1/BORRADOR.py
@@ -1,6 +1,18 @@
+"""
 print("Hola mundo desde Python")
-
 print("Hola desde mi PC para GitHub")
-
 print ("Hola mundo al fin visual studio code, python y github juntos por siempre. Gracias repositorio")
+"""
+"""
+type ("11")
+type ("abcdef")
+print (1,000,000)
+"""
 
+mensaje = ("Y ahora algo completamente diferente")
+n=17
+pi=3.1415926535897931
+print(n)
+print(pi)
+print(mensaje)
+type(mensaje)
