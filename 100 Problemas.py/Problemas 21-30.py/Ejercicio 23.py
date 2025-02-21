a63780cb8fec11ad2d115f744c167313ac067805
4er65rf <aguilarosiel117@gmail.com>
2025-02-21 14:25:34 -0600

    3213123123

--- 100 Problemas.py/Problemas 21-30.py/Ejercicio 23.py
+++ 100 Problemas.py/Problemas 21-30.py/Ejercicio 23.py
@@ -1,1 +1,14 @@
-#23.Implementar y operar con matrices.
+#23. Implementar y operar con matrices
+
+matriz1= [[0,0,0],[0,0,0],[0,0,0]]
+matriz2= [[1,1,1],[1,1,1],[1,1,1]]
+for i in range(3):
+    for j in range(3):
+        if i == j:
+            matriz1[i][j] = 1
+            matriz2[i][j] = 0
+print(matriz1,matriz2)
+
+MatrizCompleta = matriz1 + matriz2
+print(MatrizCompleta)
+
---
+++ Pruebas de python/Untitled-1.py
@@ -0,0 +1,13 @@
+meses=["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+
+enumerate(meses)
+
+for mes in enumerate (meses,1):
+    print(mes)
+
+for j in range (1,23,2):
+    print(j)
+
+
+for i in [1,3,5 ,7, 9, 11, 13, 15, 17, 19, 21, 23, 25]:
+    print(i)
