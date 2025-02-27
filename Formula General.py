8a7d99e7fc3e92c423325cd5671104d03d1b492f
4er65rf <aguilarosiel117@gmail.com>
2025-02-27 14:49:55 -0600

    32213

---
+++ Formula General.py
@@ -0,0 +1,13 @@
+def formula_general(a, b, c):
+    a = float(input("Ingrese el valor de a: ")
+    b = float(input("Ingrese el valor de b: ")
+    c = float(input("Ingrese el valor de c: "))
+    X1, X2 = formula_general(a, b, c)   
+return X1, X2
+print(X1, X2)
+  
+
+
+
+
+
