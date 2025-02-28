5c958988612221ffad5e51bacf7557424ef8b6bb
4er65rf <aguilarosiel117@gmail.com>
2025-02-28 14:35:05 -0600

    1234567890

--- Formula General.py
+++ Formula General.py
@@ -21,24 +21,60 @@ else
 #Programa corregido de la formula general
 #Autor: Valeria,Abby, Jesús
 #Fecha: 28/02/2025
+
+
 print("Este programa calcula la formula general de una ecuación cuadrática")
+
+
 a = float(input("Ingrese el valor de término cuadrático: "))
+
+
 b = float(input("Ingrese el valor de término lineal: "))
+
+
 c = float(input("Ingrese el valor de término independiente: "))
+
+
 def Disc(a,b,c):
+
+
     d = float(-b**2-(4*a*c))
+
+
     return d
+
+
 def chicharronera(a,b,c):
+
+
     x1 = (-b+Disc(a,b,c)**0.5)/(2*a)
+
+
     x2 = (-b-Disc(a,b,c)**0.5)/(2*a)
+
+
     return (x1,x2)
+
+
 if Disc(a,b,c)>=0:
+
+
     print(chicharronera(a,b,c))
+
+
 else:
+
+
     x1 = complex(-b/(2*a)+(Disc(a,b,c)*-1j))
+
+
     x2 = complex(-b/(2*a)-(Disc(a,b,c)*-1j))
+
+
     print(x1)
-    print(x2)
+
+
+    print(x2)   
 
 
 
