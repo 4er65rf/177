082c2daf238daa9affbdf233d46a63869021b1c5
4er65rf <aguilarosiel117@gmail.com>
2025-04-15 22:56:00 -0600

    11

--- Pruebas de python/.py
+++ Pruebas de python/.py
@@ -1,2 +1,25 @@
 import requests
 print(requests.__version__)
+r = requests.get('https://github.com/timeline.json')
+r = requests.post("http://httpbin.org/post")
+r = requests.put("http://httpbin.org/put")
+r = requests.delete("http://httpbin.org/delete")
+r = requests.head("http://httpbin.org/get")
+r = requests.options("http://httpbin.org/get")
+payload = {'key1': 'value1', 'key2': 'value2'}
+r = requests.get("http://httpbin.org/get", params=payload)
+print(r.url)
+r = requests.get("http://httpbin.org/get", params=payload, headers={"User-Agent": "my-app"})
+print(r.url)
+r = requests.get("http://httpbin.org/get", params=payload, headers={"User-Agent": "my-app"}, timeout=5)
+print(r.url)
+r = requests.get("http://httpbin.org/get", params=payload, headers={"User-Agent": "my-app"}, timeout=5, allow_redirects=False)
+print(r.url)
+r = requests.get("http://httpbin.org/get", params=payload, headers={"User-Agent": "my-app"}, timeout=5, allow_redirects=False, stream=True)
+print(r.url)
+r = requests.get("http://httpbin.org/get", params=payload, headers={"User-Agent": "my-app"}, timeout=5, allow_redirects=False, stream=True, verify=False)
+print(r.url)
+r = requests.get("http://httpbin.org/get", params=payload, headers={"User-Agent": "my-app"}, timeout=5, allow_redirects=False, stream=True, verify=False, cert=False)
+print(r.url)
+r = requests.get("http://httpbin.org/get", params=payload, headers={"User-Agent": "my-app"}, timeout=5, allow_redirects=False, stream=True, verify=False, cert=False, proxies={"http": "http://
+                                                                                                                                                                               
