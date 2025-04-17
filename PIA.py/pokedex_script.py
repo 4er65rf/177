2ee468b884d0279da56c5b5f0c13a2882a9f0491
4er65rf <aguilarosiel117@gmail.com>
2025-04-16 21:51:07 -0600

    11

--- PIA.py/pokedex_script.py
+++ PIA.py/pokedex_script.py
@@ -1,2 +1,18 @@
 import requests
-import re
+
+def obtener_datos(endpoint, parametro=""):
+    url = f"https://pokeapi.co/api/v2/{endpoint}/{parametro}"
+    respuesta = requests.get(url)
+
+    if respuesta.status_code == 200:
+        return respuesta.json()
+    else:
+        print("Error al hacer la petición:", respuesta.status_code)
+        return None
+
+datos = obtener_datos("pokemon", "25")  # Pikachu
+
+if datos:
+    print(f"Nombre: {datos['name'].title()}")
+    print(f"Altura: {datos['height']}")
+    print(f"Peso: {datos['weight']}")
