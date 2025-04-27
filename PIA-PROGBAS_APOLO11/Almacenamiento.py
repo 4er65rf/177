d8bab3863581dd6918e73ac83c03c6a0494aeb10
4er65rf <aguilarosiel117@gmail.com>
2025-04-26 20:23:31 -0600

    1111

--- PIA-PROGBAS_APOLO11/Almacenamiento.py
+++ PIA-PROGBAS_APOLO11/Almacenamiento.py
@@ -9,7 +9,7 @@ class PokedexExtractor:
         self.data = []
 
 
-    def extraer_datos(self, cantidad=1000):
+    def extraer_datos(self, cantidad=151): # Cambia el número de Pokémon a extraer aquí, recomendación 26 sino se saturara la API
         for id_pokemon in range(1, cantidad + 1):
             response = requests.get(f"{self.base_url}{id_pokemon}")
             if response.status_code == 200:
