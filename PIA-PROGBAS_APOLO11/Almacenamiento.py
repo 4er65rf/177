11f43b72c8e0228da6f05207385cbc6eeba484be
4er65rf <aguilarosiel117@gmail.com>
2025-04-26 20:06:30 -0600

    111

--- PIA-PROGBAS_APOLO11/Almacenamiento.py
+++ PIA-PROGBAS_APOLO11/Almacenamiento.py
@@ -8,7 +8,8 @@ class PokedexExtractor:
         self.base_url = "https://pokeapi.co/api/v2/pokemon/"
         self.data = []
 
-    def extraer_datos(self, cantidad=151):
+
+    def extraer_datos(self, cantidad=1000):
         for id_pokemon in range(1, cantidad + 1):
             response = requests.get(f"{self.base_url}{id_pokemon}")
             if response.status_code == 200:
@@ -25,11 +26,13 @@ class PokedexExtractor:
             else:
                 print(f"Error al obtener el Pokémon ID {id_pokemon}")
 
+
     def validar_datos(self):
-        patron_nombre = r'^[a-zA-Z]+$'
+        patron_nombre = r'^[a-zA-Z0-9\-]+$'  # <-- ahora acepta letras, números y guiones
         for pokemon in self.data:
             if not re.match(patron_nombre, pokemon["nombre"]):
                 raise ValueError(f"Nombre inválido detectado: {pokemon['nombre']}")
+
 
     def guardar_datos(self, filename="pokedex.csv"):
         if self.data:
@@ -40,6 +43,7 @@ class PokedexExtractor:
             print(f"Archivo {filename} guardado correctamente.")
         else:
             print("No hay datos para guardar.")
+
 
     def ejecutar(self):
         self.extraer_datos()
